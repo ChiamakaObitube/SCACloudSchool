f003f8c1049efbbe9c20bcd76b25c6f99784c58f
chiamakaobitube <obitubechiamaka@gmail.com>
2021-02-11 15:21:16 +0100

    add script to calculate the average age of men and women in the given file

--- week2/bash.sh
+++ week2/bash.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 filename=$1 #assigned the text file to a command line argument
-
-
 echo "This is the filename: $filename"
-women=$( grep "F" $1 |  grep -o -E '[0-9]+') # filters the lines in the text file having F which indicates femail
-men=$( grep "M" $1 | grep -o -E '[0-9]+') #filters the lines in the text file having F which indicates femail
+women=$( grep "F" $1 |  egrep -o '[0-9]+') # filters the lines in the text file having F and returns only the integers
+men=$( grep "M" $1 | egrep -o '[0-9]+') #filters the lines in the text file having M and retrns only the integers
+
 echo $women
 echo $men
-#women=$(echo $women); echo "$((${women// /+}))"
-#men=$(echo $men); echo "$(($women// /+}))"
 
-echo "The average of the women's age is `echo $women | awk -v RS=' ' '{sum+=$women; count++} END{print sum/count}'`"
-echo "The average of the men's age is `echo $men | awk -v RS=' ' '{sum+=$men; count++} END{print sum/count}'`"
+
+echo "The average of the women's age is `echo $women | awk -v RS=' ' '{sum+=$women; count++} END{print sum/count}'` years old"
+echo "The average of the men's age is `echo $men | awk -v RS=' ' '{sum+=$men; count++} END{print sum/count}'` years old"
 
 
